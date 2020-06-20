f09dad7948c9d9904c72fbfe914bae0905a45e0c
cdursun <cagataydursun@gmail.com>
2020-06-20 17:25:59 -0500

    Update main.R

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ source("GecoFunctions.R")
 # generate walk matrix and runs RWR to generate neighborhood with RWR scores
 nbhd <- generateNbhd("input_files", 40)
 
-# Generate different top N neighborhoods using nbhd data frame, 2nd parameter is the prefix for output file name
+# Generates different top N neighborhoods using nbhd data frame, 2nd parameter is the prefix for output file name
 # for different top N parameters provide following parameters
 # top_Ggs=c(150, 250), top_Gps=c(150, 250), top_Pgs=c(150, 250), top_Pps=c(150, 250)
 trimTopNbhd(nbhd, "nbhd")
